87c11ff8dd89c91c4cf029ac3eea5f1e32eaeb8b
Mohammad Shahbazi <mohammadshahbazi03zn@gmail.com>
2019-12-16 03:04:03 -0800

    shopping card styles changed

--- src/components/productCartPage/CartItem.js
+++ src/components/productCartPage/CartItem.js
@@ -1,28 +1,30 @@
-import  React from 'react'
-import { FaArrowDown,FaArrowUp,FaTrash } from 'react-icons/fa'
+import React from 'react'
+import { FaArrowDown, FaArrowUp, FaTrash } from 'react-icons/fa'
 import { removeItem, increaseTheCount, decreaseTheCount } from '../../actions/cart'
 import formatMoney from '../../utilites/formatMoney'
-const CartItem=(props)=>{
-    const handleRemoveItem=()=>{
+const CartItem = (props) => {
+    const handleRemoveItem = () => {
         props.dispatch(removeItem(props.id))
     }
-    const handleIncreaseTheCount=()=>{
+    const handleIncreaseTheCount = () => {
         props.dispatch(increaseTheCount(props.id))
     }
-    const handleDecreaseTheCount=()=>{
-        props.dispatch(decreaseTheCount(props.id)) 
+    const handleDecreaseTheCount = () => {
+        props.dispatch(decreaseTheCount(props.id))
     }
     return (
         <div className="cart-item">
-            <img className="cart-item__img" src={props.path} alt={props.name}/>
-            <h2 className="cart-item__name">{props.name}</h2>
-            <p className="cart-item__price">{formatMoney(`${props.cost -((props.cost * props.off) / 100)}`)} تومان</p>
-            <div className="cart-item__counter">
-            <FaArrowUp className="hover" onClick={handleIncreaseTheCount} />
-            <span>{props.count}</span> 
-            <FaArrowDown className="hover" onClick={handleDecreaseTheCount}  />
+            <img className="cart-item__img" src={props.path} alt={props.name} />
+            <div className="cart-item__body">
+                <h2 className="cart-item__name">{props.name}</h2>
+                <p className="cart-item__price">{formatMoney(`${props.cost - ((props.cost * props.off) / 100)}`)} تومان</p>
+                <div className="cart-item__counter">
+                    <FaArrowUp className="hover" onClick={handleIncreaseTheCount} />
+                    <span className="cart-item__counter--count">{props.count}</span>
+                    <FaArrowDown className="hover" onClick={handleDecreaseTheCount} />
+                </div>
+                <FaTrash className="hover cart-item__trash" onClick={handleRemoveItem} />
             </div>
-            <FaTrash className="hover"  onClick={handleRemoveItem} />
         </div>
     )
 }
--- src/components/productCartPage/ProductCardPage.js
+++ src/components/productCartPage/ProductCardPage.js
@@ -3,20 +3,22 @@ import { useGlobalState } from '../../context/Context'
 import CartItem from './CartItem'
 import { clearCart } from '../../actions/cart'
 import MoneySummery from './MoneySummery'
-const ProductCardPage = ()=>{
-    const [{cart},dispatch]=useGlobalState()
-    const handleClearCart=()=>{
+const ProductCardPage = () => {
+    const [{ cart }, dispatch] = useGlobalState()
+    const handleClearCart = () => {
         dispatch(clearCart())
     }
     return (
         <div className="container mt-10">
-            {
+            <div className="cart-item__container">
+                {
+                    cart.map((item) => <CartItem key={item.id} dispatch={dispatch} {...item} />)
+                }
+            </div>
 
-                cart.map((item)=><CartItem key={item.id} dispatch={dispatch} {...item} />)
-            }
             <div className="flex">
                 <button onClick={handleClearCart} className="btn btn--buy">خالی کردن کارت</button>
-                <MoneySummery items={cart} />    
+                <MoneySummery items={cart} />
             </div>
         </div>
     )
